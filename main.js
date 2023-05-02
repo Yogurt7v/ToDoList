92bf70ec6be1f0f00c02f3daed6555d3371f6bbc
Yogurt7v <egorov-2k@yandex.ru>
2023-05-02 20:28:01 +0300

    Second strp

--- main.js
+++ main.js
@@ -35,15 +35,72 @@
     return list;
   }
 
+  function createTodoItem(name) {
+    let item = document.createElement("li");
+    let buttonGrounp = document.createElement("div");
+    let doneButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+
+    item.classList.add(
+      "list-group0-item",
+      "d-flex",
+      "justify-content-space-between",
+      "align-items-center"
+    );
+    item.textContent = name;
+
+    buttonGrounp.classList.add("btn-group", "btn-group-sm");
+    doneButton.classList.add("btn", "btn-success");
+    doneButton.textContent = "Окей";
+    deleteButton.classList.add("btn", "btn-danger");
+    deleteButton.textContent = "Удалить";
+
+    buttonGrounp.append(doneButton);
+    buttonGrounp.append(deleteButton);
+    item.append(buttonGrounp);
+
+    return {
+      item,
+      doneButton,
+      deleteButton,
+    };
+  }
+
   document.addEventListener("DOMContentLoaded", function () {
     let container = document.getElementById("todo-app");
 
     let todoAppTitle = createAppTitle("Список дел");
     let todoItemForm = createToDoItemForm();
     let todoList = createToDOList();
+    let todoItems = [
+      createTodoItem("Придти домой"),
+      createTodoItem("Ботанить"),
+    ];
 
     container.append(todoAppTitle);
     container.append(todoItemForm.form);
     container.append(todoList);
+
+    todoItemForm.form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      if (!todoItemForm.input.value) {
+        return;
+      }
+
+      let todoItem = createTodoItem(todoItemForm.input.value);
+
+      todoItem.doneButton.addEventListener("click", function () {
+        todoItem.item.classList.toggle("list-group-item-success");
+      });
+      todoItem.deleteButton.addEventListener("click", function () {
+        if (confirm("Уверен?")) {
+          todoItem.item, this.remove();
+        }
+      });
+
+      todoList.append(todoItem.item);
+      todoItemForm.input.value = "";
+    });
   });
 })();
